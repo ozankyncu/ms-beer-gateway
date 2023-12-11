77ac8c272bbbd8791e42ed2660ad714b42c4c909
Ozan Koyuncu <ozankyncu@gmail.com>
2023-12-11 21:26:48 +0300

    Initial commit

---
+++ src/main/java/com/kyncu/msbeergateway/MsBeerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kyncu.msbeergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBeerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBeerGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyncu/msbeergateway/MsBeerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyncu.msbeergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
