b7b873785f9a6a134b0882616b00125c8273b79b
Ozan Koyuncu <ozankyncu@gmail.com>
2023-12-15 21:30:36 +0300

    Route configured for beer services

--- src/test/java/com/kyncu/msbeergateway/MsBeerGatewayApplicationTests.java
+++ src/test/java/com/kyncu/msbeergateway/MsBeerGatewayApplicationTests.java
@@ -1,13 +1,12 @@
 package com.kyncu.msbeergateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MsBeerGatewayApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
